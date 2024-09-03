9ab2efee59692aa2e9b49d5783a8eaef22397c43
dndqlszzz <mandarin1999@naver.com>
2024-09-03 23:08:55 +0900

    2606_Virus

--- Week2/Virus2606.java
+++ Week2/Virus2606.java
@@ -16,43 +16,50 @@ public class Virus2606{
         int V = Integer.parseInt(reader.readLine());
         // 간선 개수
         int E = Integer.parseInt(reader.readLine());
-        // 그래프 틀을 생성
+        // 그래프 틀 생성
         ArrayList<ArrayList<Integer>>graph = new ArrayList<>();
-        for (int i = 0; i <= V; i++) {
+        for (int i = 0; i < V; i++) {
             graph.add(new ArrayList<>());
         }
+        boolean[] visited = new boolean[V];
 
         // 그래프 인접리스트로 구현 각 노드에 연결된 노드를 삽입, 그래프 구현
         for(int i = 0 ; i < E ; i++){
             String[] str = reader.readLine().split(" ");
+
             int first = Integer.parseInt(str[0]);
             int last = Integer.parseInt(str[1]);
-            graph.get(first).add(last);
-            graph.get(last).add(first);
+
+            // get은 index를 파라미터로 갖는데 인덱스는 PC번호-1이므로
+            graph.get(first-1).add(last);
+            graph.get(last-1).add(first);
         }
-        // 방문체크를 위한 배열, 인덱스가 노드 번호로 사용할 것이므로 V+1만큼 생성
-        boolean[] visited = new boolean[V+1];
-        bfs_list(1,graph,visited);
 
-        System.out.println(Arrays.toString(visited));
+        bfs(1, graph, visited);
 
+        int count = 0;
+        // 1로부터 감염된 PC의 개수를 구해야 하므로 i=1부터
+        for(int i=1;i<visited.length;i++){
+            if(visited[i]) count++;
+        }
+        System.out.println(count);
+    }
+    public static void bfs(int v, ArrayList<ArrayList<Integer>> graph, boolean[] visited){
+        //index가 PC번호-1 이므로 -1을 해주었다.
+        Queue<Integer> queue = new LinkedList<>();
+        visited[v-1] = true;
+        queue.add(v);
 
+        while(!queue.isEmpty()){
+            v = queue.poll();
+
+            for (int vertex : graph.get(v-1)){
+                if(!visited[vertex-1]){
+                    visited[vertex-1] = true;
+                    queue.add(vertex);
+                }
+            }
+        }
 
     }
-    public static void bfs_list(int v, ArrayList<ArrayList<Integer>> graph, boolean[] visited){
-         Queue<Integer> q = new LinkedList<>();
-         int n = graph.size()-1;
-         q.add(v);
-         visited[v] = true;
-         while (!q.isEmpty()){
-             v = q.poll();
-             System.out.println(v + " ");
-
-             for(int node : graph.get(v)){
-                 q.add(node);
-                 visited[node] = true;
-             }
-         }
-    }
-
 }
