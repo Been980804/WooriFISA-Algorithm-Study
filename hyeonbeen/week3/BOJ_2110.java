d3335358e0350b2366ef8d337bcb45b95adc4535
Been980804 <dlgusqls980804@naver.com>
2024-09-11 08:24:43 +0900

    공유기(미해결)

--- hyeonbeen/week3/BOJ_2110.java
+++ hyeonbeen/week3/BOJ_2110.java
@@ -27,12 +27,39 @@ public class BOJ_2110 {
 
         Arrays.sort(x);
 
-        min = 1;                // 최소거리
-        max = x[N-1] - x[0];    // 최대거리
+        min = 1;                // 최소 거리
+        max = x[N-1] - x[0];    // 최대 거리
         ans = 0;
         while(min <= max){
             int mid = (max + min) / 2;
-        
+            if(canInstall(min)){
+                ans = mid - 1;
+                min = mid + 1;
+            } else{
+                max = mid - 1;
+            }
         }
+
+        System.out.println(ans);
+    }
+
+    public static boolean canInstall(int distance){
+        int cnt = 1;
+        int prev = x[0];
+
+        for(int i = 1; i < N; i++){
+            if(x[i] - prev >= distance){
+                cnt++;
+                prev = x[i];
+                if(cnt >= C){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
+
+/*
+ * mid 가 공유기간의 최소 거리로 생각
+ */
