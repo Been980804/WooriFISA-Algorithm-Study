d87963d5124f08a3795b1887239917d8854a1c60
2oo1s <90839206+2oo1s@users.noreply.github.com>
2024-09-27 08:31:51 +0900

    Create BOJ_3568.java

---
+++ 2oo1s/BOJ_3568.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(", ");
+
+        String com = str[0].split(" ")[0];  // 기본 변수형
+        str[0] = str[0].split(" ")[1];
+        str[str.length - 1] = str[str.length - 1].replace(";", "");
+
+
+        for (String s : str) {
+            StringBuilder opt = new StringBuilder();
+            String end = "";
+
+            for (int j = 0; j < s.length(); j++) {
+                char cur = s.charAt(j);
+
+                if (cur == '*' || cur == '&')
+                    opt.append(cur);
+                else if (cur == '[')
+                    opt.append(']');
+                else if
+                (cur == ']') opt.append('[');
+                else if (('A' <= cur && cur <= 'Z') || ('a' <= cur && cur <= 'z'))
+                    end += cur;
+            }
+            System.out.println(com + opt.reverse() + " " + end + ";");
+        }
+    }
+}
